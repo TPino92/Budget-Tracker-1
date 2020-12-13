c40101cb6a026cc74746b32c5d7514a65846a919
Jesus Manteca <jesusmanteca@gmail.com>
2020-12-13 12:21:08 -0600

    added service worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,7 +2,6 @@
 let db;
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
-
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
     // save a reference to the database 
@@ -10,7 +9,6 @@ request.onupgradeneeded = function (event) {
     // create an object store (table) called `new_budget_entry`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_budget_entry', { autoIncrement: true });
 };
-
 // upon a successful 
 request.onsuccess = function (event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
@@ -19,15 +17,13 @@ request.onsuccess = function (event) {
     // check if app is online, if yes run uploadTransaction(); function to send all local db data to api
     if (navigator.onLine) {
         // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadTransaction();
+        uploadTransaction();
     }
 };
-
 request.onerror = function (event) {
     // log error here
     console.log(event.target.errorCode);
 };
-
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
@@ -39,3 +35,44 @@ function saveRecord(record) {
     // add record to your store with add method
     transactionObjectStore.add(record);
 }
+function uploadTransaction() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+
+    // access your pending object store
+    const transactionObjectStore = transaction.objectStore('new_budget_entry');
+
+    // get all records from store and set to a variable
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+                    const transactionObjectStore = transaction.objectStore('new_budget_entry');
+                    // clear all items in your store
+                    transactionObjectStore.clear();
+                })
+                .catch(err => {
+                    // set reference to redirect back here
+                    console.log(err);
+                });
+        }
+    };
+}
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
